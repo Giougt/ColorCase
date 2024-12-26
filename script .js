6e8f5a22fcb99e2c69767eb392ab936eddfab828
Giougt <alexandre18golf@gmail.com>
2024-12-26 19:18:57 +0100

    add input color and fucntion choiceColor

--- script .js
+++ script .js
@@ -29,14 +29,24 @@ Object.assign(allCase.style, {
     gap: "2px" 
 });
 
-//align cube in center of body 
+
+//body style 
 Object.assign(document.body.style, {
+    margin: "0"
+});
+
+
+//parent container
+const wrapper = document.createElement("div");
+//style 
+Object.assign(wrapper.style, {
     display: "flex",
-    justifyContent: "center", 
-    alignItems: "center", 
-    height: "100vh", 
-    margin: "0", 
-    backgroundColor: "#727874" 
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    margin: "0",
+    backgroundColor: "#727874",
+    gap: "20px"
 });
 
 // create 100 cubes 
@@ -51,5 +61,31 @@ for (let i = 0; i < 100; i++) {
     allCase.appendChild(test);
 }
 
-// append in document
-document.body.appendChild(allCase);
+// append in container
+wrapper.appendChild(allCase);
+
+// Create input color
+const colorPicker = document.createElement("input");
+colorPicker.type = "color";
+Object.assign(colorPicker.style, {
+    width: "50px",
+    height: "50px",
+    border: "none",
+    borderRadius: "8px",
+    cursor: "pointer"
+});
+
+// add input container parent
+wrapper.appendChild(colorPicker);
+
+// append in document all
+document.body.appendChild(wrapper);
+
+//stock color
+let color = "";
+
+function choiceColor(){
+    colorPicker.addEventListener("change", (event) => {
+        color = event.target.value
+    })
+};
